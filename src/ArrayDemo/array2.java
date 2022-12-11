f1d18e4a151c56a8ef313c9014adee0833b92cf1
ADMIN <ADMIN@DESKTOP-RKJOLBL>
2022-12-11 13:52:30 +0530

    First commit

---
+++ src/ArrayDemo/array2.java
@@ -0,0 +1,28 @@
+package ArrayDemo;
+
+public class array2 {
+
+	public static void main(String[] args) {
+		int []array = new int[10];
+		array[0]=10;
+		array[1]=20;
+		array[2]=30;
+		array[3]=40;
+		array[4]=50;
+		array[5]=60;
+		array[6]=70;
+		array[7]=80;
+		array[8]=90;
+		array[9]=100;
+		
+		
+		
+		for (int var:array)
+		{
+			System.out.println("squre of "+var+" is "+var*var);
+		}
+		
+
+	}
+
+}
